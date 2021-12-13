3efdef244b582feca4bfc2cfe485f089c807b227
Antoine FERRON [BitLogiK] <antoine.ferron@bitlogik.fr>
2021-12-13 14:56:13 +0100

    New version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,10 @@
 
 # pyWeb3 changes log
 
+## 0.1.3
+
+* Improve queries reliability
+
 ## 0.1.2
 
 * Improve response reading reliability
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@
 
 from setuptools import setup, find_packages
 
-VERSION = "0.1.2"
+VERSION = "0.1.3"
 
 with open("README.md") as readme_file:
     readme = readme_file.read()
